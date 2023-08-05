372507a05c1914aec26b69f6d40efaf1023d5570
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-08-05 01:30:38 -0600

    :recycle: Better base URL handling
    Replace animation players with new ones using the proper source.
    Bypass replacement logic if there is no base URL.

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,18 @@ const deck = new Reveal();
 deck.initialize({hash: true, slideNumber: true});
 
 (function prependBase() {
+  const base = import.meta.env.BASE_URL;
+  if (!base) {
+    return;
+  }
   document.querySelectorAll('motion-canvas-player').forEach(player => {
     let url = player.getAttribute('src');
     if (url?.startsWith('/')) {
-      url = import.meta.env.BASE_URL + url.slice(1);
-      player.setAttribute('src', url);
+      url = base + url.slice(1);
+      const newElement = document.createElement('motion-canvas-player');
+      newElement.setAttribute('auto', player.getAttribute('auto') ?? 'true');
+      newElement.setAttribute('src', url);
+      player.replaceWith(newElement);
     }
   });
 })();
