4e70f4439e607fd551cf9fe43b7f147bf22b22f2
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-08-05 00:33:40 -0600

    :tada: Initial commit

---
+++ animations/src/anim2/scenes/scene.tsx
@@ -0,0 +1,12 @@
+import {makeScene2D, Rect} from '@motion-canvas/2d';
+import {createRef, loop, makeRef, waitFor} from '@motion-canvas/core';
+
+export default makeScene2D(function* (view) {
+  const rect = createRef<Rect>();
+  // Create your animations here
+  view.add(<Rect size={100} ref={rect} fill={'green'}></Rect>)
+
+  yield loop(Infinity, () => rect().position.y(100, 1).to(0, 1));
+
+  yield* waitFor(6);
+});
